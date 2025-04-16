b17dea11d1e8f451ef6a0b28cb7fb696577da8bf
Debjyoti Paul <11320427+DebjyotiPaul@users.noreply.github.com>
2025-04-16 09:38:52 +0530

    ..

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ def main():
      server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
      #server_socket.accept()[0].sendall(b"HTTP/1.1 200 OK\r\n\r\n")
      conn, addr = server_socket.accept()
-     client_msg = conn.recv(1024).decode().split(" ")
+     client_msg = conn.recv(4096).decode().split(" ")
      print(client_msg)
      if client_msg[1] == "/":
         conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
