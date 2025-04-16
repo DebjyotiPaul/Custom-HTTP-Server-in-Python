887baac1147507b8846acabcf90b0b03e2732cbd
Debjyoti Paul <11320427+DebjyotiPaul@users.noreply.github.com>
2025-04-16 09:33:24 +0530

    ..

--- app/main.py
+++ app/main.py
@@ -8,7 +8,14 @@ def main():
     # Uncomment this to pass the first stage
     #
      server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-     server_socket.accept()[0].sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+     #server_socket.accept()[0].sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+     conn, addr = server_socket.accept()
+     client_msg = conn.recv(4096).decode().split(" ")
+     print(client_msg)
+     if client_msg[1] == "/":
+        conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+     else:
+        conn.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
 
 if __name__ == "__main__":
     main()
